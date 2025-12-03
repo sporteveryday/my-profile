c71487c793ba27dae6783302bb7b6b747f658ea3
sporteveryday <673572034@qq.com>
2025-12-03 18:18:47 +0800

    配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.github.io', // REPLACE_ME: Your GitHub Pages URL
+  site: 'https://sporteveryday.github.io', // REPLACE_ME: Your GitHub Pages URL
   base: '/my-profile', // REPLACE_ME: Your repository name
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
